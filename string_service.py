44d4628f7c9242ab6c020f4bed2461aa87e335aa
Happy.Zhang <ihzlpy@gmail.com>
2017-05-18 17:28:02 +0800

    update string_service.py

--- string_service.py
+++ string_service.py
@@ -12,7 +12,7 @@ class ReverseHandler(tornado.web.RequestHandler):
     def get(self, input):
         self.write(input[::-1])
 
-class WarpHandler(tornado.web.RequestHandler):
+class WrapHandler(tornado.web.RequestHandler):
     def post(self):
         text = self.get_argument('text')
         width = self.get_argument('width', 40)
