856369fff62485a403715646534fa1b3c594313e
Happy.Zhang <ihzlpy@gmail.com>
2017-05-19 16:35:53 +0800

    用 MySQ:

--- 4/burts_books_rwdb_single.py
+++ 4/burts_books_rwdb_single.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import os.path
 
 import tornado.auth
@@ -14,111 +15,120 @@ import torndb
 define("port", default=8000, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
-	def __init__(self):
-		handlers = [
-			(r"/", MainHandler),
-			(r"/recommended/", RecommendedHandler),
-			(r"/books/([0-9Xx\-]+)", BookHandler),
-			(r"/edit/([0-9Xx\-]+)", BookEditHandler),
-			(r"/add", BookEditHandler)
-		]
-		settings = dict(
-			template_path=os.path.join(os.path.dirname(__file__), "templates"),
-			static_path=os.path.join(os.path.dirname(__file__), "static"),
-			ui_modules={"Book": BookModule},
-			debug=True,
-			)
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/recommended/", RecommendedHandler),
+            (r"/books/([0-9Xx\-]+)", BookHandler),
+            (r"/edit/([0-9Xx\-]+)", BookEditHandler),
+            (r"/add", BookEditHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            ui_modules={"Book": BookModule},
+            debug=True,
+        )
         conn = torndb.Connection("192.168.119.55:3306", "bookstore", user="root", password="Happy@1234")
         self.db = conn
-		tornado.web.Application.__init__(self, handlers, **settings)
+
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-
-		self.render(
-			"index.html",
-			page_title = "Burt's Books | Home",
-			header_text = "Welcome to Burt's Books!",
-		)
+    def get(self):
+        self.render(
+            "index.html",
+            page_title = "Burt's Books | Home",
+            header_text = "Welcome to Burt's Books!",
+        )
 
 class BookHandler(tornado.web.RequestHandler):
-	def get(self, isbn=None):
-		if isbn:
+    def get(self, isbn=None):
+        if isbn:
             sql = "SELECT * FROM book WHERE isbn = " % isbn
-            book = self.application.db.query(sql)
-			if book:
-				self.render("one_book.html",
-					page_title="Burt's Books | " + book['title'],
-					header_text=book['title'],
-					book=book)
-				return
-		self.set_header(404)
-		return
+            book = self.application.db.get(sql)
+            if book:
+                self.render("one_book.html",
+                    page_title="Burt's Books | " + book['title'],
+                    header_text=book['title'],
+                    book=book)
+                return
+            self.set_header(404)
+        return
 
 class BookEditHandler(tornado.web.RequestHandler):
-	def get(self, isbn=None):
-		book = dict()
-		if isbn:
-            sql = "SELECT * FROM book WHERE isbn = " % isbn
-            book = self.application.db.query(sql)
-		self.render("book_edit.html",
-			page_title="Burt's Books",
-			header_text="Edit book",
-			book=book)
+    def get(self, isbn=None):
+        book = dict()
+        if isbn:
+            sql = "SELECT * FROM book WHERE isbn = '%s'" % isbn
+            print(sql)
+            book = self.application.db.get(sql)
+        print(book)
+        self.render("book_edit.html",
+            page_title="Burt's Books",
+            header_text="Edit book",
+            book=book)
 
-	def post(self, isbn=None):
-		import time
-		book_fields = ['isbn', 'title', 'subtitle', 'image', 'author',
-			'date_released', 'description']
+    def post(self, isbn=None):
+        import time
+        book_fields = ['isbn', 'title', 'subtitle', 'image', 'author', 'date_released', 'description']
+        if isbn:
+            sql = "SELECT * FROM book WHERE isbn = '%s'" % isbn
+            book = self.application.db.get(sql)
+        for key in book_fields:
+            #print(book)
+            book[key] = self.get_argument(key, None)
 
-		if isbn:
-            sql = "SELECT * FROM book WHERE isbn = " % isbn
-            book = self.application.db.query(sql)
-		for key in book_fields:
-			book[key] = self.get_argument(key, None)
+        title = book['title']
+        subtitle = book['subtitle']
+        image = book['image']
+        author = book['author']
+        date_added = book['date_added']
+        date_released = book['date_released']
+        description = book['description']
+        isbn = book['isbn']
 
-		if isbn:    # 更新
-			#coll.save(book)
-            sql = "UPDATE book SET title = %s, subtitle = %s, image = %s, author = %s, date_added = %s, date_released = %s, description = %s"
-                    % (title, subtitle, image, author, date_added, date_released, description)
+        if isbn:    # 更新
+            sql = "UPDATE book SET title = '%s', subtitle = '%s', image = '%s', author = '%s', date_added = '%s', date_released = '%s', description = '%s' WHERE isbn = '%s'" % (title, subtitle, image, author, date_added, date_released, description, isbn)
             self.application.db.execute(sql)
-		else:       # 插入
-			book['date_added'] = int(time.time())
+        else:       # 插入
+            book['date_added'] = int(time.time())
+            date_added = book['date_added']
             sql = "INSERT INTO book(title, subtitle, image, author, date_added, date_released, isbn, description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-			self.application.db.insert(sql, title, subtitle, image, author, date_added, date_released, isbn, description)
-		self.redirect("/recommended/")
+            self.application.db.insert(sql, title, subtitle, image, author, date_added, date_released, isbn, description)
+        self.redirect("/recommended/")
 
 class RecommendedHandler(tornado.web.RequestHandler):
-	def get(self):
-		sql = "SELECT * FROM book"
+    def get(self):
+        sql = "SELECT * FROM book"
         books = self.application.db.query(sql)
-		self.render(
-			"recommended.html",
-			page_title = "Burt's Books | Recommended Reading",
-			header_text = "Recommended Reading",
-			books = books
-		)
+        self.render(
+            "recommended.html",
+            page_title = "Burt's Books | Recommended Reading",
+            header_text = "Recommended Reading",
+            books = books
+        )
 
 class BookModule(tornado.web.UIModule):
-	def render(self, book):
-		return self.render_string(
-			"modules/book.html",
-			book=book,
-		)
+    def render(self, book):
+        return self.render_string(
+            "modules/book.html",
+            book=book,
+        )
 
-	def css_files(self):
-		return "css/recommended.css"
+    def css_files(self):
+        return "css/recommended.css"
 
-	def javascript_files(self):
-		return "js/recommended.js"
+    def javascript_files(self):
+        return "js/recommended.js"
 
 
 def main():
-	tornado.options.parse_command_line()
-	http_server = tornado.httpserver.HTTPServer(Application())
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-	main()
+    main()
